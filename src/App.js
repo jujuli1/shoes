3ea96f6121c1968177617ed60cc326e8ea614d0d
LEFEVRE <lefevrejulien@live.fr>
2024-08-09 18:41:05 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import main from './composants/main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <main/>
     </div>
   );
 }
---
+++ src/composants/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const main = () => {
+    return (
+        <div>
+            <h1>coucou</h1>
+        </div>
+    );
+};
+
+export default main;
